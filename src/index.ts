cb06439f4c02c56368a292bda2ba63092d99dc94
drewp@bigasterisk.com <drewp@bigasterisk.com>
2020-01-29 00:43:56 -0800

    update deps, add more components to the page

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,6 @@ export { DomBind } from "@polymer/polymer/lib/elements/dom-bind.js";
 export { StreamedGraph } from "streamed-graph";
 export { BigastMultiformatClock} from "bigast-multiformat-clock";
 export { BigastGraphValue} from "bigast-graph-value";
-export { BigastCalendar} from "bigast-calendar';
+export { BigastCalendar} from "bigast-calendar";
+export { ForecastIo} from "forecast-io";
+export { CountdownView } from 'countdown-view'; 
